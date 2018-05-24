b4a7599e5ec6bbdaff05ec07fb46fdff50babfc0
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-24 23:24:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -8,23 +8,12 @@ board = [" "," "," "," "," "," "," "," "," "]
 
 
 
-def display_board(board)
-  print "   "
-  print "|"
-  print "   "
-  print  "|"
-  puts "   "
-  puts "-----------"
-  print "   "
-  print "|"
-  print "   "
-  print  "|"
-  puts "   "
-  puts "-----------"
-  print "   "
-  print "|"
-  print "   "
-  print  "|"
-  puts "   "
+def display_board()
+puts " #{board[0]} | #{board[1]} | #{board[2]} "
+puts "-----------"
+puts " #{board[3]} | #{board[4]} | #{board[5]} "
+puts "-----------"
+puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
   
 end
