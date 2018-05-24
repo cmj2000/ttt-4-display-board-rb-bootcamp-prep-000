822cffdf886f627b831c128331702e124ddf7a36
c.mj2000@yahoo.com <c.mj2000@yahoo.com>
2018-05-24 23:18:41 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,2 +1,30 @@
 # Define display_board that accepts a board and prints
 # out the current state.
+
+
+board = [" "," "," "," "," "," "," "," "," "]
+
+
+
+
+
+def display_board(board)
+  print "   "
+  print "|"
+  print "   "
+  print  "|"
+  puts "   "
+  puts "-----------"
+  print "   "
+  print "|"
+  print "   "
+  print  "|"
+  puts "   "
+  puts "-----------"
+  print "   "
+  print "|"
+  print "   "
+  print  "|"
+  puts "   "
+  
+end
